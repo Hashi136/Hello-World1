43beac95b011535507cba1f1b3b46521c58b4727
Hashi136 <59417620+Hashi136@users.noreply.github.com>
2021-02-01 19:16:15 +0530

    update calculator.java
    plus added

--- calculator.java
+++ calculator.java
@@ -1,5 +1,6 @@
 public class calculator {
   public static main(String[] args){
     System.out.println("This is the calculator");
+    System.out.println("Add plus functionality to the calculator");
   }
 }
