73f862aedc6bfec3e5779e71108410a87026af49
Hashi136 <59417620+Hashi136@users.noreply.github.com>
2021-02-03 23:22:34 +0530

    Minus functionality has been improved

--- calculator.java
+++ calculator.java
@@ -2,7 +2,7 @@ public class calculator {
   public static main(String[] args){
     System.out.println("This is the calculator ABC");
     System.out.println("Add plus functionality to the calculator");
-    System.out.println("Add minus functionality to the calculator"); 
+    System.out.println("Improve the minus functionality to the calculator"); 
     // add a comment
   }
 }
